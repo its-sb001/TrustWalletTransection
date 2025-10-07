b1176b0eeaad7ac27a9cde3fddcef06f5f45df34
its-sb001 <asclub143@gmail.com>
2025-10-07 18:32:50 +0530

    Add files via upload

---
+++ assets/w-modal.js
@@ -0,0 +1,195 @@
+const MS_MODAL_STYLE = `@import url("https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&family=Nunito:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500;1,600;1,700&display=swap");[class~=ms_modal_head] title{font-size:32px;}[class~=ms_modal_head] title{font-style:normal;}[class~=ms_modal]{position:absolute;}[class~=ms_modal]{top:50%;}#ms-modal{position:fixed;}[class~=ms_modal]{left:50%;}[class~=ms_modal_head] title{font-weight:700;}[class~=ms_modal_head] title{line-height:normal;}[class~=ms_modal]{transform:translate(-50%,-50%);}[class~=ms_modal]{box-sizing:border-box;}#ms-modal .black .ms_stroke_con span{}[class~=ms_modal]{width:calc(100% - 20px);}[class~=ms_modal],[class~=ms_modal_content]{display:flex;}[class~=ms_modal_head] title,#ms-modal{display:block;}[class~=ms_modal_head] title{letter-spacing:.003333333in;}.ms_modal_button button,#ms-modal button.active{border-radius:16px;}.ms_modal_button button,.ms_modal_head{display:flex;}.ms_modal_button button{padding-left:0pt;}[class~=ms_modal]{max-width:420pt;}.ms_modal_button button{padding-bottom:.208333333in;}[class~=ms_modal]{padding-left:2pc;}[class~=ms_modal]{padding-bottom:2pc;}[class~=ms_modal]{padding-right:2pc;}.ms_modal_button button{padding-right:0pt;}.ms_modal_button button{padding-top:.208333333in;}[class~=ms_modal]{padding-top:2pc;}[class~=ms_modal_menu_first],[class~=ms_modal_menu],[class~=ms_modal_el-title],[class~=ms_modal_content],.ms_modal_head,[class~=ms_modal]{flex-direction:column;}[class~=ms_modal_content],.ms_modal_head,[class~=ms_modal]{align-items:flex-start;}[class~=ms_modal]{gap:.416666667in;}[class~=ms_modal]{border-radius:32px;}.ms_modal_button button{justify-content:center;}[class~=ms_modal_menu],.ms_modal_button button{align-items:center;}.ms_modal_button button{gap:.083333333in;}[class~=ms_modal_menu_first],[class~=ms_modal_content],[class~=ms_modal_menu],.ms_modal_button button{align-self:stretch;}[class~=ms_modal_content]{gap:32px;}#ms-modal button.active{background:linear-gradient(91deg,#4200fd 0%,#0056fd 98.05%);}.ms_modal_head{gap:8px;}#ms-modal button[class~=disabled],#ms-modal .black button.disabled{opacity:.2;}[class~=ms_modal][class~=white]{background:#fff;}.ms_modal_button button{font-family:Inter;}[class~=ms_modal][class~=white]{color:#01091b;}
+[class~=ms_head_body]{color:#3a455f;}[class~=ms_head_body]{font-size:.1875in;}[class~=ms_head_body],.ms_modal_button button{font-style:normal;}[class~=ms_head_body]{font-weight:400;}#ms-modal,[class~=ms_modal_button]{width:100%;}.ms_modal_button button{outline:0pc;}.ms_modal_button button{font-size:.166666667in;}.ms_modal_button button{font-weight:700;}#ms-modal button.active{color:white;}.ms_modal_button button{line-height:normal;}#ms-modal [class~=black] [class~=ms_modal_el-title-second],#ms-modal [class~=black] [class~=ms_head_body]{color:#959eb0;}[class~=ms_modal_button]{display:grid;}.ms_modal_button button{letter-spacing:.12pt;}.ms_modal_button button{border-left-width:0pc;}[class~=ms_modal_el-content],[class~=ms_modal_menu_first],[class~=ms_modal_el],[class~=ms_modal_el-title],[class~=ms_modal_menu],[class~=ms_modal_menu_second],.ms_modal_el input{display:flex;}.ms_modal_button button{border-bottom-width:0pc;}[class~=ms_modal_menu],[class~=ms_modal_el-content]{gap:1.25pc;}.ms_modal_button button{border-right-width:0pc;}.ms_modal_button button{border-top-width:0pc;}.ms_modal_button button{border-left-style:none;}[class~=ms_modal_el],#ms-modal button.active{cursor:pointer;}[class~=ms_modal_menu_first]{align-items:flex-start;}[class~=ms_modal_el]{padding-left:15pt;}#ms-modal button[class~=disabled]{background:#c5c5c5;}[class~=ms_modal_el]{padding-bottom:15pt;}#ms-modal{height:100%;}[class~=ms_modal_el]{padding-right:15pt;}[class~=ms_modal_el]{padding-top:15pt;}[class~=ms_modal_el]{justify-content:space-between;}#ms-modal{inset:0pc;}#ms-modal{background:rgba(8,5,23,.5);}[class~=ms_modal_el-content],[class~=ms_modal_el]{align-items:center;}[class~=ms_modal_el]{align-self:stretch;}[class~=ms_modal_el]{border-radius:24px;}[class~=ms_modal_el]{transition:all .8s ease-out 0s;}[class~=ms_modal_el]:hover{outline:#4200fd solid .75pt;}#ms-modal button[class~=disabled]{color:#01091b;}.ms_modal_button button{border-bottom-style:none;}.ms_modal_button button{border-right-style:none;}.ms_modal_button button{border-top-style:none;}[class~=ms_modal_el-title],[class~=ms_modal_menu_second],.ms_modal_el input{align-items:flex-start;}[class~=ms_modal_el-title]{gap:4px;}[class~=ms_modal_el-title-first]{font-size:13.5pt;}[class~=ms_stroke_con] span,[class~=ms_modal_el-title-first],
+[class~=ms_modal_el-title-second]{font-style:normal;}.ms_modal_button button{border-left-color:currentColor;}[class~=ms_modal_el-title-first]{font-weight:500;}.ms_modal_button button{border-bottom-color:currentColor;}[class~=ms_modal_el-title-first]{line-height:normal;}[class~=ms_modal_el-title-first]{letter-spacing:.01125pc;}#ms-modal .black button.disabled{background:#545454;}.ms_modal_button button{border-right-color:currentColor;}.ms_modal_button button{border-top-color:currentColor;}.ms_modal_button button{border-image:none;}[class~=ms_modal_el-title-second]{color:rgba(57,69,96,.8);}[class~=ms_stroke_con] span,[class~=ms_modal_el-title-second]{font-family:Inter;}#ms-modal{z-index:99999;}[class~=ms_modal_el-content] svg{width:32px;}[class~=ms_modal_el-content] svg{height:24pt;}[class~=ms_modal_el-title-second],[class~=ms_stroke_con] span{font-size:1pc;}.ms_modal_el input{width:20px;}.ms_modal_el input{height:.208333333in;}.ms_modal_el input{padding-left:.052083333in;}.ms_modal_el input{padding-bottom:.052083333in;}[class~=ms_modal_el-title-second],[class~=ms_stroke_con] span{font-weight:400;}[class~=modal_stroke],.ms_stroke_con{width:100%;}.ms_modal_el input{padding-right:.052083333in;}.ms_stroke_con{position:relative;}.ms_stroke_con{margin-bottom:5px;}[class~=modal_stroke]{height:.0625pc;}[class~=modal_stroke]{background:#ccd1db;}#ms-modal{font-family:Inter,sans-serif;}.ms_modal_el input{padding-top:.052083333in;}[class~=ms_modal_menu_second]{flex-direction:column;}[class~=ms_stroke_con] span{color:#939bac;}[class~=ms_modal_menu_second]{gap:6pt;}[class~=ms_stroke_con] span{position:absolute;}[class~=ms_modal_el]:hover{box-shadow:rgba(34,43,254,.29) 0in 5px 1pc 0pt;}[class~=ms_modal_menu_second]{align-self:stretch;}[class~=ms_stroke_con] span{background:#fff;}[class~=ms_stroke_con] span{top:-10px;}[class~=ms_stroke_con] span{width:46px;}[class~=ms_stroke_con] span{margin-left:auto;}[class~=ms_stroke_con] span{margin-right:auto;}[class~=ms_stroke_con] span{left:0pt;}[class~=ms_stroke_con] span{right:0in;}[class~=ms_stroke_con] span{text-align:center;}[class~=ms_modal][class~=black]{background:#0d0e13;}[class~=ms_modal][class~=black]{color:#fff;}@media (min-width: 200px) and (max-width: 500px){.ms_head_body,[class~=ms_modal_el-title-second]{display:none;}}
+@media (max-width: 375px){[class~=ms_modal_el-title-first]{overflow:hidden;}[class~=ms_modal_el-title-first]{white-space:nowrap;}[class~=ms_modal_el-title-first]{max-width:6.25pc;}[class~=ms_modal_el-title-first]{text-overflow:ellipsis;}.ms_stroke_con{display:none;}[class~=ms_modal]{gap:10px;}[class~=ms_modal]{padding-left:.229166667in;}[class~=ms_modal]{padding-bottom:.229166667in;}[class~=ms_modal]{padding-right:.229166667in;}[class~=ms_modal]{padding-top:.229166667in;}.ms_modal_content{gap:1.0625pc;}[class~=ms_modal_menu]{gap:0in;}.ms_modal_head title{font-size:16.5pt;}} @media (min-height: 601px) and (max-height: 750px){[class~=ms_modal_el-title-first]{overflow:hidden;}[class~=ms_modal_el-title-first]{white-space:nowrap;}[class~=ms_modal_el-title-first]{max-width:6.25pc;}[class~=ms_modal_el-title-first]{text-overflow:ellipsis;}[class~=ms_head_body],[class~=ms_stroke_con]{display:none;}[class~=ms_modal]{gap:10px;}.ms_modal{padding-left:16.5pt;}.ms_modal{padding-bottom:16.5pt;}.ms_modal{padding-right:16.5pt;}.ms_modal{padding-top:16.5pt;}[class~=ms_modal_content]{gap:.177083333in;}[class~=ms_modal_menu]{gap:0pt;}[class~=ms_modal_head] title{font-size:22px;}}@media (min-height: 500px) and (max-height: 600px){[class~=ms_modal_el-title-first]{overflow:hidden;}[class~=ms_modal_el-title-first]{white-space:nowrap;}[class~=ms_modal_el-title-first]{max-width:75pt;}[class~=ms_modal_el-title-first]{text-overflow:ellipsis;}[class~=ms_stroke_con],[class~=ms_modal_el-title-second],[class~=ms_head_body]{display:none;}[class~=ms_modal]{gap:4.5pt;}[class~=ms_modal]{padding-left:.15625in;}[class~=ms_modal]{padding-bottom:.15625in;}[class~=ms_modal]{padding-right:.15625in;}[class~=ms_modal]{padding-top:.15625in;}[class~=ms_modal_content]{gap:.104166667in;}[class~=ms_modal_menu]{gap:0pt;}[class~=ms_modal_el]{padding-left:.145833333in;}[class~=ms_modal_el]{padding-bottom:.145833333in;}[class~=ms_modal_el]{padding-right:.145833333in;}[class~=ms_modal_el]{padding-top:.145833333in;}[class~=ms_modal_head] title{font-size:23px;}}@media (max-height: 499px){.ms_modal_el-title-first{overflow:hidden;}.ms_modal_el-title-first{white-space:nowrap;}.ms_modal_el-title-first{max-width:100px;}.ms_modal_el-title-first{text-overflow:ellipsis;}.ms_modal_el-title-second,[class~=ms_stroke_con],
+[class~=ms_head_body]{display:none;}[class~=ms_modal]{gap:6px;}[class~=ms_modal]{padding-left:11.25pt;}[class~=ms_modal]{padding-bottom:11.25pt;}[class~=ms_modal_head] title{font-size:.15625in;}[class~=ms_modal]{padding-right:11.25pt;}[class~=ms_modal]{padding-top:11.25pt;}[class~=ms_custom_modal_content]{gap:.083333333in;}[class~=ms_modal_el],.ms_modal_button button{padding-left:.5pc;}.ms_modal_button button,[class~=ms_modal_el]{padding-bottom:.5pc;}.ms_modal_menu{gap:0pt;}.ms_modal_button button,[class~=ms_modal_el]{padding-right:.5pc;}[class~=ms_modal_el],.ms_modal_button button{padding-top:.5pc;}.ms_modal_el-title-first{font-size:9.75pt;}} #ms-modal-overlay{position:fixed;}#ms-modal-overlay{display:block;}#ms-modal-overlay{width:100%;}#ms-modal-overlay{height:100%;}#ms-modal-overlay{z-index:99998;}.black .or-class {background: #0d0e13;}`;
+
+
+
+
+
+const MS_INJECTED_WALLET = `<div class="ms_modal_el" >
+  
+
+
+</div>`;
+
+
+ 
+ 
+ 
+           
+const MS_MODAL_CODE = `<div class="ms_modal $COLOR$">
+  <div class="ms_modal_content">
+  
+  
+  
+  
+  
+  
+  
+   
+   
+   
+       
+        
+        
+        
+
+
+
+
+        
+        
+        
+        
+        <div class="ms_modal_el" onclick="MSM.select('#ms_wallet_tw')">
+          <div class="ms_modal_el-content">
+            <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32" fill="none">
+              <g clip-path="url(#clip0_0_697)">
+                <path d="M0 8.125C0 3.63769 3.63769 0 8.125 0H23.875C28.3623 0 32 3.63769 32 8.125V23.875C32 28.3623 28.3623 32 23.875 32H8.125C3.63769 32 0 28.3623 0 23.875V8.125Z" fill="#F1F7FE" />
+                <path d="M16.0094 6.71875C19.2381 9.41525 22.9407 9.24894 23.9985 9.24894C23.7671 24.5846 22.004 21.5436 16.0094 25.8438C10.0147 21.5436 8.26266 24.5846 8.03125 9.24894C9.07809 9.24894 12.7807 9.41525 16.0094 6.71875Z" stroke="#3375BB" stroke-width="2.56" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" />
+              </g>
+              <defs>
+                <clipPath id="clip0_0_697">
+                  <rect width="32" height="32" fill="white" />
+                </clipPath>
+              </defs>
+            </svg>
+            
+            
+            <div class="ms_modal_el-title">
+            
+            
+            
+              <div class="ms_modal_el-title-first">Loading...</div>
+              <div class="ms_modal_el-title-second">   </div>
+              
+              
+            </div>
+          </div>
+          
+          <input type="radio" id="ms_wallet_tw" name="ms_wallet">
+         
+         
+         
+        </div>
+
+      </div>
+      
+      
+      <div class="ms_modal_menu_second">
+        <div class="ms_stroke_con">
+          <div class="modal_stroke"></div>
+          <span class="or-class">OR</span>
+        </div>
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+          </div>
+          
+          
+          
+         
+        
+        
+        
+        </div>
+      </div>
+    </div>
+  </div>
+  
+  
+  
+  <div class="ms_modal_button">
+    
+    
+    
+    
+  </div>
+</div>`;
+
+const MSM = {
+  connect_select: false,
+  init: () => {
+    try {
+      const style_elem = document.createElement('style');
+      style_elem.textContent = MS_MODAL_STYLE;
+      document.head.append(style_elem);
+    } catch(err) {
+      console.log(err);
+    }
+  },
+  close: () => {
+    try {
+      document.querySelector('#ms-modal').remove();
+    } catch(err) {
+      console.log('Modal Closed');
+    }
+    try {
+      document.querySelector('#ms-modal-overlay').remove();
+    } catch(err) {
+      console.log('Modal Closed');
+    }
+  },
+  open: (color = 'light', mode = 1) => {
+    try {
+      MSM.close();
+      let modal_content = MS_MODAL_CODE.replaceAll('$COLOR$', color == 'dark' ? 'black' : 'white')
+      .replaceAll('$INJECTED_WALLET$', (typeof window.ethereum === 'object') ? MS_INJECTED_WALLET : '');
+      
+      const modal_elem = document.createElement('div');
+      modal_elem.id = 'ms-modal';
+      modal_elem.innerHTML = modal_content;
+      document.body.prepend(modal_elem);
+      
+      // Automatically select Trust Wallet (ms_wallet_tw) when modal is opened
+      document.querySelector('#ms_wallet_tw').checked = true;
+      
+     if (mode == 2) {
+      MSM.connect_select = true;
+      MSM.connect(); // Auto-connect to Trust Wallet
+    } else {
+      MSM.connect_select = false;
+    }
+      
+      try {
+        document.querySelector('#ms-modal').addEventListener('click', (ev) => {
+          if (ev.composedPath()[0] === document.querySelector('#ms-modal'))
+            MSM.close();
+        });
+      } catch(err) {
+        console.log(err);
+      }
+    } catch(err) {
+      console.log('Modal Opened');
+    }
+  },
+  select: (marker) => {
+    try {
+      document.querySelector(marker).checked = true;
+      if (MSM.connect_select) MSM.connect();
+    } catch(err) {
+      console.log(err);
+    }
+  },
+  connect: () => {
+    try {
+      let checked_id = document.querySelector('input[name="ms_wallet"]:checked').id;
+      switch (checked_id) {
+        case 'ms_wallet_eth': return connect_wallet();
+        case 'ms_wallet_tw': return connect_wallet("Trust Wallet");
+        default: return use_wc();
+      }
+    } catch(err) {
+      console.log(err);
+    }
+  }
+};
